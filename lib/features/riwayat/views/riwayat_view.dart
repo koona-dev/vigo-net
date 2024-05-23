c30f0ddade185914e279780b30fd269e25904171
widnyana18 <mangwidnyana2d.art@gmail.com>
2024-05-23 20:21:56 +0800

    [fix]: modify status widget

--- lib/features/riwayat/views/riwayat_view.dart
+++ lib/features/riwayat/views/riwayat_view.dart
@@ -3,6 +3,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:intl/intl.dart';
 import 'package:isp_app/common/widgets/error.dart';
 import 'package:isp_app/features/order_product/controller/order_product_controller.dart';
+import 'package:isp_app/model/order.dart';
 
 class RiwayatView extends ConsumerStatefulWidget {
   const RiwayatView({Key? key}) : super(key: key);
@@ -14,6 +15,32 @@ class RiwayatView extends ConsumerStatefulWidget {
 }
 
 class _RiwayatViewState extends ConsumerState<RiwayatView> {
+  Widget _generatedStatusWidget(Orders order) {
+    Widget _pendingStatusWidget = Chip(
+      label: Text('Menunggu'),
+      backgroundColor: Colors.yellow,
+    );
+
+    Widget _successStatusWidget = Chip(
+      label: Text('Berhasil'),
+      backgroundColor: Colors.green,
+    );
+
+    Widget _cancelStatusWidget = Chip(
+      label: Text('Dibatalkan'),
+      backgroundColor: Colors.red,
+    );
+
+    switch (order.status) {
+      case OrderStatus.menunggu:
+        return _pendingStatusWidget;
+      case OrderStatus.berhasil:
+        return _successStatusWidget;
+      default:
+        return _cancelStatusWidget;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     final ordersData = ref.watch(orderDataProvider);
@@ -60,10 +87,7 @@ class _RiwayatViewState extends ConsumerState<RiwayatView> {
                             child: Column(
                               crossAxisAlignment: CrossAxisAlignment.start,
                               children: [
-                                Chip(
-                                  label: Text('Menunggu'),
-                                  backgroundColor: Colors.yellow,
-                                ),
+                                _generatedStatusWidget(order),
                                 SizedBox(height: 12),
                                 Text(item.productName),
                                 SizedBox(height: 8),
